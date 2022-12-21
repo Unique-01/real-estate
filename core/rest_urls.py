982d1b7c70a1e16f37102cd84392fbad6c8e1929
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2022-12-21 17:00:39 +0100

    Added API documentation with drf-spectacular

--- core/rest_urls.py
+++ core/rest_urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import routers
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
 router.register('property-list', views.PropertyViewSet, basename='property')
@@ -14,4 +15,11 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('myprofile/', views.UserApiView.as_view()),
     path('register/', views.RegisterApiView.as_view()),
+
+    # DOCUMENTATION
+    path('docs/default', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
 ]
+
